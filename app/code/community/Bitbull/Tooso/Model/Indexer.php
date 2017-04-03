7dd41b6ead997048eb3ef758addfad6e2f21f337
Fabio Gollinucci <fbgolly@gmail.com>
2017-04-03 15:26:06 +0200

    Custom exporter, index store one by one
    - list store grouped by lang
    - custom exporter, only text attributes
    - lunch index for each store

--- app/code/community/Bitbull/Tooso/Model/Indexer.php
+++ app/code/community/Bitbull/Tooso/Model/Indexer.php
@@ -18,6 +18,11 @@ class Bitbull_Tooso_Model_Indexer
      */
     protected $_logger = null;
 
+    /**
+     * @var Mage_ImportExport_Model_Export_Adapter_Csv
+    */
+    protected $_writer;
+
     public function __construct()
     {
         $this->_client = Mage::helper('tooso')->getClient();
@@ -32,17 +37,22 @@ class Bitbull_Tooso_Model_Indexer
     */
     public function rebuildIndex()
     {
-        try {
-            $this->_client->index($this->_getCsvContent());
-        } catch (Exception $e) {
-            $this->_logger->logException($e);
+        $data = $this->_getStoresByLang();
+        foreach ($data as $lang => $stores) {
+            foreach ($stores as $storeId) {
+                try {
+                    $this->_client->index($this->_getCsvContent($storeId));
+                } catch (Exception $e) {
+                    $this->_logger->logException($e);
 
-            return false;
+                    return false;
+                }
+            }
         }
 
         return true;
     }
-    
+
     /**
      * Clean Tooso Index
      *
@@ -58,11 +68,11 @@ class Bitbull_Tooso_Model_Indexer
     /**
      * return string
     */
-    protected function _getCsvContent()
+    protected function _getCsvContent($storeId)
     {
+        /*
         $this->_logger->debug('Start generating CSV content');
 
-        /** @var $model Mage_ImportExport_Model_Export */
         $model = Mage::getModel('importexport/export');
 
         $this->_logger->debug('Export Model class: ' . get_class($model));
@@ -77,6 +87,95 @@ class Bitbull_Tooso_Model_Indexer
 
         $this->_logger->debug('End generating CSV content');
 
-        return $csvContent;
+        return $csvContent;*/
+
+        // Elenco di attributi standard di Magento da non tradurre
+        $excludeAttributes = array(
+            'image_label',
+            'old_id',
+            'small_image_label',
+            'thumbnail_label',
+            'uf_product_link',
+            'url_path',
+        );
+
+        $attributes = array('sku' => 'SKU');
+        $headers = array('sku' => 'sku');
+
+        // Recupero tutti gli attributi di tipo testo, cercando di escludere quelli di sistema
+        $attributesCollection = Mage::getResourceModel('catalog/product_attribute_collection')
+            ->addFieldToFilter('frontend_input', array('in' => array('textarea', 'text')))
+            ->addFieldToFilter('backend_model', array('null' => true))
+            ->addFieldToFilter('backend_type', array('neq' => 'static'))
+            ->addFieldToFilter('attribute_code', array('nin' => $excludeAttributes))
+        ;
+
+        $productCollection = Mage::getModel('catalog/product')
+            ->getCollection()
+            ->addStoreFilter($storeId)
+            ->addAttributeToSelect('sku')
+        ;
+
+        foreach ($attributesCollection as $attribute) {
+            $attributes[$attribute->getAttributeCode()] = $attribute->getFrontendLabel();
+            $headers[$attribute->getAttributeCode()] = $attribute->getAttributeCode();
+
+            $productCollection->addAttributeToSelect($attribute->getAttributeCode());
+
+            // Occorre mettere in join esplicitamente gli attributi in base allo store id
+            // addStoreFilter sulla collection sembra impattare solo l'associazione prodotto <-> website
+            $productCollection->joinAttribute(
+                $attribute->getAttributeCode(),
+                'catalog_product/' . $attribute->getAttributeCode(),
+                'entity_id',
+                null,
+                'left',
+                $storeId
+            );
+        }
+
+        $this->_getWriter()->setHeaderCols($headers);
+
+        foreach ($productCollection as $product) {
+            $row = array();
+            foreach ($attributes as $attributeCode => $attributeLabel) {
+                $row[$attributeCode] = $product->getData($attributeCode);
+            }
+
+            $this->_getWriter()->writeRow($row);
+        }
+
+        return $this->_getWriter()->getContents();
+    }
+
+    /**
+     * Get stores grouped by lang code
+     * @return array stores
+     */
+    protected function _getStoresByLang()
+    {
+        $stores = array();
+        $collection = Mage::getModel('core/store')->getCollection();
+        foreach ($collection as $store) {
+            $lang= Mage::getStoreConfig('general/locale/code', $store->getId());
+            if (!isset($stores[$lang])) {
+                $stores[$lang] = array();
+            }
+            array_push($stores[$lang], $store->getId());
+        }
+
+        return $stores;
+    }
+
+    /**
+     * @return Mage_ImportExport_Model_Export_Adapter_Csv
+    */
+    protected function _getWriter()
+    {
+        if (!$this->_writer) {
+            $this->_writer = Mage::getModel('importexport/export_adapter_csv', array());
+        }
+
+        return $this->_writer;
     }
 }

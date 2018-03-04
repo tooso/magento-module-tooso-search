a4ba4a4217a9aabc7430fa28c36cef3b2ded718f
Fabio Melen <fabio.melen@gmail.com>
2018-03-04 21:34:37 +0100

    add all user settings

--- app/code/community/Bitbull/Tooso/Block/Suggestion/LibraryInit.php
+++ app/code/community/Bitbull/Tooso/Block/Suggestion/LibraryInit.php
@@ -11,10 +11,20 @@ class Bitbull_Tooso_Block_Suggestion_LibraryInit extends Bitbull_Tooso_Block_Sug
 
     protected function _toHtml()
     {
-        $inputSelector = $this->_helper->getSuggestionInputSelector();
-        $apiKey = $this->_helper->getApiKey();
-        $zindex = $this->_helper->getSuggestionZIndex();
-        $buckets = $this->_helper->getSuggestionBuckets();
+        $inputSelector          = $this->_helper->getSuggestionInputSelector();
+        $apiKey                 = $this->_helper->getApiKey();
+        $zindex                 = $this->_helper->getSuggestionZIndex();
+        $buckets                = $this->_helper->getSuggestionBuckets();
+        $noCache                = $this->_helper->getSuggestionNoCache();
+        $language               = $this->_helper->getSuggestionLanguage();
+        $uid                    = $this->_helper->getSuggestionUID();
+        $limit                  = $this->_helper->getSuggestionLimit();
+        $groupBy                = $this->_helper->getSuggestionGroupBy();
+        $submitOnSelect         = $this->_helper->getSuggestionSubmitOnSelect();
+        $autoCompleteMinChars   = $this->_helper->getSuggestionAutocompleteMinChars();
+        $autocompleteMaxHeight  = $this->_helper->getSuggestionAutocompleteMaxHeight();
+        $autocompleteWidth      = $this->_helper->getSuggestionAutocompleteWidth();
+        $autocompleteOnSelect   = $this->_helper->getSuggestionAutocompleteOnSelect();
         $this->_logger->debug('initializing suggestion library');
 
         ob_start();
@@ -26,10 +36,26 @@ class Bitbull_Tooso_Block_Suggestion_LibraryInit extends Bitbull_Tooso_Block_Sug
                         var element = $('<?=$inputSelector?>');
                         if(element){
                             element.attr('data-ts', '')
-                                .attr('data-ts-api-key', '<?=$apiKey?>')
-                                .attr('data-ts-zindex', <?=$zindex?>)
-                                .attr('data-ts-buckets', '<?=$buckets?>')
-                                .ts();
+                                .ts({
+                                <? if (!empty($apiKey))     : ?>apiKey:'<?=$apiKey?>',<? endif; ?>
+                                <? if (!empty($language))   : ?>language: '<?=$language?>',<? endif; ?>
+                                <? if (!empty($uid))        : ?>uid: '<?=$uid?>',<? endif; ?>
+                                <? if (!empty($buckets))    : ?>buckets: '<?=$buckets?>',<? endif; ?> 
+                                <? if (!empty($limit))      : ?>limit: '<?=$limit?>',<? endif; ?>
+                                <? if (!empty($groupBy))    : ?>groupBy: <?=$groupBy?>,<? endif; ?>
+                                <? if (!empty($noCache))    : ?>noCache: <?=$noCache?>,<? endif; ?>
+                                    autocomplete: {
+                                    <? if ($autocompleteOnSelect) : ?>
+                                        onSelect: <?=$autocompleteOnSelect?>,
+                                    <? elseif ($submitOnSelect) : ?>
+                                        onSelect: function() { this.form.submit(); },
+                                    <? endif; ?>
+                                        minChars: <?=($autoCompleteMinChars ?: 'undefined')?>,
+                                        maxHeight: <?= ($autocompleteMaxHeight ?: 'undefined')?>,
+                                        width: <?= ($autocompleteWidth ?: 'undefined')?>,
+                                        zIndex: <?= ($zindex ?: 'undefined')?>,
+                                    },
+                                });
                         }else{
                             console.error("Tooso: Suggestion search input not found");
                         }

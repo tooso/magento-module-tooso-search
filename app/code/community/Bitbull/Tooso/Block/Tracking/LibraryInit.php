f4347d0bcf471a6e25dcc1a4f302e132ece57001
Fabio Gollinucci <fbgolly@gmail.com>
2018-10-04 17:20:18 +0200

    Remove storeCurrency product tracking, set currency tracking init param

--- app/code/community/Bitbull/Tooso/Block/Tracking/LibraryInit.php
+++ app/code/community/Bitbull/Tooso/Block/Tracking/LibraryInit.php
@@ -14,12 +14,16 @@ class Bitbull_Tooso_Block_Tracking_LibraryInit extends Bitbull_Tooso_Block_Track
         $debugMode = $this->_helper->isDebugMode();
         $trackingKey = $this->_helper->getTrackingKey();
         $cookieDomain = $this->_helper->getCookieDomain('auto');
+        $currency = $this->_helper->getCurrencyCode();
 
         ob_start();
         ?>
         <script id='<?=self::SCRIPT_ID?>' type='text/javascript'>
             window.ta=window.ta||function(){(ta.q=ta.q||[]).push(arguments)};ta.l=+new Date;
             ta('create', '<?=$trackingKey?>', '<?=$cookieDomain?>');
+            <?php if($currency): ?>
+            ta('set','currencyCode','<?=$currency?>');
+            <?php endif; ?>
             <?php if($debugMode): ?>
             ta('set','debug','true');
             <?php endif; ?>
--- app/code/community/Bitbull/Tooso/Helper/Tracking.php
+++ app/code/community/Bitbull/Tooso/Helper/Tracking.php
@@ -252,7 +252,6 @@ class Bitbull_Tooso_Helper_Tracking extends Mage_Core_Helper_Abstract
             'name' => $product->getName(),
             'brand' => $product->getBrand(),
             'price' => $product->getFinalPrice(),
-            'storeCurrency' => Mage::app()->getStore()->getCurrentCurrencyCode(),
             'quantity' => 1
         ];
 
@@ -266,6 +265,23 @@ class Bitbull_Tooso_Helper_Tracking extends Mage_Core_Helper_Abstract
         }
 
         return $trackingProductParams;
+    }
+
+    /**
+     * Get store currency code
+     *
+     * @param null $store
+     * @return string
+     * @throws Mage_Core_Model_Store_Exception
+     */
+    public function getCurrencyCode($store = 'blablabla'){
+        $code = null;
+        try{
+            $code = Mage::app()->getStore($store)->getCurrentCurrencyCode();
+        } catch (Mage_Core_Model_Store_Exception $e){
+            Mage::helper('tooso/log')->logException($e);
+        }
+        return $code;
     }
 
     /**
